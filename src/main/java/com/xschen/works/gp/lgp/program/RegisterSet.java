5e52094d23c54485062e98b4554d5372cd0e1408
xschen <xs0040@gmail.com>
2017-04-29 12:07:38 +0800

    Implement register set

---
+++ src/main/java/com/xschen/works/gp/lgp/program/RegisterSet.java
@@ -0,0 +1,117 @@
+package com.xschen.works.gp.lgp.program;
+
+
+import com.xschen.works.gp.services.RandEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by xschen on 28/4/2017.
+ * Containers for the mutable registers
+ */
+public class RegisterSet {
+   private final List<Register> registers = new ArrayList<>();
+   private final List<Double> weights = new ArrayList<>();
+
+   public RegisterSet(){
+
+   }
+
+   public boolean isReadOnly(){
+      return false;
+   }
+
+   public RegisterSet makeCopy(){
+      RegisterSet clone = new RegisterSet();
+      for(int i=0; i < registers.size(); ++i){
+         clone.registers.add(registers.get(i).makeCopy());
+         clone.weights.add(weights.get(i));
+      }
+      return clone;
+   }
+
+   public double weightSum() {
+      if(weights.isEmpty()){
+         return 0;
+      }
+      return weights.stream().reduce((a, b ) -> a + b).get();
+   }
+
+   public Register anyOther(Register excluded, RandEngine rand){
+      double weightSum = weightSum();
+      for(int attempts = 0; attempts < 10; attempts++){
+         double r = weightSum * rand.uniform();
+         double accSum = 0;
+         for(int i=0; i < registers.size(); ++i){
+            accSum += weights.get(i);
+            if(r <= accSum){
+               if(registers.get(i) == excluded){
+                  break;
+               } else {
+                  return registers.get(i);
+               }
+            }
+         }
+      }
+
+      return excluded;
+   }
+
+   public Register get(int index){
+      return registers.get(index);
+   }
+
+   public int size() {
+      return registers.size();
+   }
+
+   public void add(Register register, double weight) {
+      register.setConstant(isReadOnly());
+      register.setIndex(registers.size());
+      registers.add(register);
+      weights.add(weight);
+   }
+
+   public void add(double value, double weight){
+      Register register = new Register();
+      register.setValue(value);
+      add(register, weight);
+   }
+
+   @Override
+   public String toString() {
+      StringBuilder sb = new StringBuilder();
+
+      for(int i=0; i < registers.size(); ++i){
+         if(i != 0){
+            sb.append("\r\n");
+         }
+         sb.append("register[").append(i).append("]: ").append(registers.get(i));
+      }
+      return sb.toString();
+   }
+
+
+   @Override public boolean equals(Object o) {
+      if (this == o)
+         return true;
+      if (o == null || getClass() != o.getClass())
+         return false;
+
+      RegisterSet that = (RegisterSet) o;
+
+      if (!registers.equals(that.registers))
+         return false;
+      return weights.equals(that.weights);
+
+   }
+
+
+   @Override public int hashCode() {
+      int result = registers.hashCode();
+      result = 31 * result + weights.hashCode();
+      return result;
+   }
+}
--- src/test/java/com/xschen/works/gp/lgp/program/RegisterSetUnitTest.java
+++ src/test/java/com/xschen/works/gp/lgp/program/RegisterSetUnitTest.java
@@ -3,7 +3,6 @@ package com.xschen.works.gp.lgp.program;
 
 import com.xschen.works.gp.services.RandEngine;
 import com.xschen.works.gp.services.SimpleRandEngine;
-import org.assertj.core.api.AssertionsForClassTypes;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -14,43 +13,43 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
  * Created by xschen on 28/4/2017.
  * Unit test for the ConstantSet
  */
-public class ConstantSetUnitTest {
+public class RegisterSetUnitTest {
 
-   private ConstantSet constantSet;
+   private RegisterSet registerSet;
    private RandEngine randEngine;
 
    @BeforeMethod
    public void setUp(){
-      constantSet = new ConstantSet();
+      registerSet = new RegisterSet();
       randEngine = new SimpleRandEngine();
 
       for(int i=0; i < 10; i++) {
-         constantSet.add(0.1 * i, 1.0);
+         registerSet.add(0.1 * i, 1.0);
       }
    }
 
    @Test
    public void test_readOnly(){
-      for(int i=0; i < constantSet.size(); ++i){
-         AssertionsForClassTypes.assertThat(constantSet.get(i).isConstant()).isTrue();
+      for(int i=0; i < registerSet.size(); ++i){
+         assertThat(registerSet.get(i).isConstant()).isFalse();
       }
    }
 
    @Test
    public void test_size(){
-      assertThat(constantSet.size()).isEqualTo(10);
+      assertThat(registerSet.size()).isEqualTo(10);
    }
 
    @Test
    public void test_makeCopy(){
-      assertThat(constantSet.makeCopy()).isEqualTo(constantSet);
+      assertThat(registerSet.makeCopy()).isEqualTo(registerSet);
    }
 
    @Test
    public void test_anyOther(){
       for(int i=0; i < 4; ++i) {
-         Register register = constantSet.anyOther(constantSet.get(i), randEngine);
-         assertThat(register).isNotEqualTo(constantSet.get(i));
+         Register register = registerSet.anyOther(registerSet.get(i), randEngine);
+         assertThat(register).isNotEqualTo(registerSet.get(i));
       }
    }
 
