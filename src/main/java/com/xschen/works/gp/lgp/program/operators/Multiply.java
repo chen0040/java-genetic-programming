efd45d040ae5acc92222605242609d9359869aa5
xschen <xs0040@gmail.com>
2017-04-30 11:16:33 +0800

    Implement divide operator Implement multiply operator

--- src/main/java/com/xschen/works/gp/lgp/program/operators/Multiply.java
+++ src/main/java/com/xschen/works/gp/lgp/program/operators/Multiply.java
@@ -9,26 +9,27 @@ import com.xschen.works.gp.lgp.program.Register;
 /**
  * Created by xschen on 30/4/2017.
  */
-public class Sine extends Operator {
+public class Multiply extends Operator {
 
-   private static final long serialVersionUID = 612876391355964531L;
+   private static final long serialVersionUID = -3206913752183532206L;
 
 
-   public Sine() {
-      super("sin");
+   public Multiply(){
+      super("*");
    }
 
-
    @Override public Operator makeCopy() {
-      Sine clone = new Sine();
+      Multiply clone = new Multiply();
       clone.copy(this);
       return clone;
    }
 
 
    @Override public OperatorExecutionStatus execute(Register operand1, Register operand2, Register destination_register) {
-      double x = operand1.getValue();
-      destination_register.setValue(Math.sin(x));
+      double x1 = operand1.getValue();
+      double x2 = operand2.getValue();
+      destination_register.setValue(x1 * x2);
+
       return OperatorExecutionStatus.LGP_EXECUTE_NEXT_INSTRUCTION;
    }
 }
