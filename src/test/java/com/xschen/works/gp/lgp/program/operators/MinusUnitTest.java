2700b769b9c0e004a616d6cab69bbd668abacd20
xschen <xs0040@gmail.com>
2017-05-01 14:56:04 +0800

    Implement + and - operators

---
+++ src/test/java/com/xschen/works/gp/lgp/program/operators/MinusUnitTest.java
@@ -0,0 +1,32 @@
+package com.xschen.works.gp.lgp.program.operators;
+
+
+import com.xschen.works.gp.lgp.enums.OperatorExecutionStatus;
+import com.xschen.works.gp.lgp.program.Operator;
+import com.xschen.works.gp.lgp.program.Register;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+
+/**
+ * Created by xschen on 1/5/2017.
+ */
+public class MinusUnitTest {
+
+   @Test
+   public void test(){
+      Operator op = new Minus();
+
+      Register reg1 = new Register();
+      Register reg2 = new Register();
+      Register destination = new Register();
+
+      reg1.setValue(2);
+      reg2.setValue(2);
+
+      assertThat(op.execute(reg1, reg2, destination)).isEqualTo(OperatorExecutionStatus.LGP_EXECUTE_NEXT_INSTRUCTION);
+
+      assertThat(destination.getValue()).isEqualTo(0);
+   }
+}
